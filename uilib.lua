c3b9e41e000b368fa4c9290ba89df7fd76282edd
Griffin // vykzs <89820610+vyks@users.noreply.github.com>
2022-04-05 14:39:12 -0400

    Update uilib.lua

--- uilib.lua
+++ uilib.lua
@@ -6,36 +6,36 @@ function WindowTable:CreateWindow()
 	uilibtut.Parent = game.CoreGui
 	uilibtut.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 	uilibtut.ResetOnSpawn = false
-	local mainframe = Instance.new("Frame")
-	mainframe.Name = "mainframe"
-	mainframe.Parent = uilibtut
-	mainframe.AnchorPoint = Vector2.new(0.5, 0.5)
-	mainframe.BackgroundColor3 = Color3.fromRGB(61, 64, 71)
-	mainframe.BorderColor3 = Color3.fromRGB(216, 222, 233)
-	mainframe.ClipsDescendants = true
-	mainframe.Position = UDim2.new(0.407529712, 0, 0.694610715, 0)
-	mainframe.Size = UDim2.new(0, 330, 0, 240)
-	local sidebar = Instance.new("Frame")
-	sidebar.Name = "sidebar"
-	sidebar.Parent = mainframe
-	sidebar.BackgroundColor3 = Color3.fromRGB(55, 57, 63)
-	sidebar.BorderSizePixel = 0
-	sidebar.Position = UDim2.new(0, 0, 0, 20)
-	sidebar.Size = UDim2.new(0, 85, 0, 219)
+	local Main = Instance.new("Frame")
+	Main.Name = "Main"
+	Main.Parent = uilibtut
+	Main.AnchorPoint = Vector2.new(0.5, 0.5)
+	Main.BackgroundColor3 = Color3.fromRGB(61, 64, 71)
+	Main.BorderColor3 = Color3.fromRGB(216, 222, 233)
+	Main.ClipsDescendants = true
+	Main.Position = UDim2.new(0.5, 0, 0.5, 0)
+	Main.Size = UDim2.new(0, 380, 0, 310)
+	local tabHolder = Instance.new("Frame")
+	tabHolder.Name = "tabHolder"
+	tabHolder.Parent = Main
+	tabHolder.BackgroundColor3 = Color3.fromRGB(55, 57, 63)
+	tabHolder.BorderSizePixel = 0
+	tabHolder.Position = UDim2.new(0, 0, 0, 25)
+	tabHolder.Size = UDim2.new(0, 100, 0, 285)
 	local UIListLayout = Instance.new("UIListLayout")
-	UIListLayout.Parent = sidebar
+	UIListLayout.Parent = tabHolder
 	UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
 	UIListLayout.Padding = UDim.new(0, 5)
 	local UIPadding = Instance.new("UIPadding")
-	UIPadding.Parent = sidebar
+	UIPadding.Parent = tabHolder
 	UIPadding.PaddingLeft = UDim.new(0, 5)
 	UIPadding.PaddingTop = UDim.new(0, 5)
 	local Title = Instance.new("Frame")
 	Title.Name = "Title"
-	Title.Parent = mainframe
+	Title.Parent = Main
 	Title.BackgroundColor3 = Color3.fromRGB(55, 57, 63)
 	Title.BorderSizePixel = 0
-	Title.Size = UDim2.new(0, 85, 0, 20)
+	Title.Size = UDim2.new(0, 100, 0, 25)
 	local TitleText = Instance.new("TextLabel")
 	TitleText.Name = "TitleText"
 	TitleText.Parent = Title
@@ -49,24 +49,25 @@ function WindowTable:CreateWindow()
 	TitleText.TextColor3 = Color3.fromRGB(216, 222, 233)
 	TitleText.TextSize = 16.000
 	TitleText.TextXAlignment = Enum.TextXAlignment.Left
-	local TextButton_2 = Instance.new("TextButton")
-	TextButton_2.Parent = mainframe
-	TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	TextButton_2.BackgroundTransparency = 1.000
-	TextButton_2.BorderColor3 = Color3.fromRGB(27, 42, 53)
-	TextButton_2.Position = UDim2.new(0, 314, 0, 0)
-	TextButton_2.Size = UDim2.new(0, 16, 0, 14)
-	TextButton_2.Font = Enum.Font.Gotham
-	TextButton_2.Text = "x"
-	TextButton_2.TextColor3 = Color3.fromRGB(210, 60, 65)
-	TextButton_2.TextSize = 14.000
+	local Close = Instance.new("TextButton")
+	Close.Name = "Close"
+	Close.Parent = Main
+	Close.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Close.BackgroundTransparency = 1.000
+	Close.BorderColor3 = Color3.fromRGB(27, 42, 53)
+	Close.Position = UDim2.new(0, 364, 0, 0)
+	Close.Size = UDim2.new(0, 16, 0, 14)
+	Close.Font = Enum.Font.Gotham
+	Close.Text = "x"
+	Close.TextColor3 = Color3.fromRGB(210, 60, 65)
+	Close.TextSize = 14.000
 	local Pages = Instance.new("Frame")
 	Pages.Name = "Pages"
-	Pages.Parent = mainframe
+	Pages.Parent = Main
 	Pages.BackgroundColor3 = Color3.fromRGB(55, 57, 63)
 	Pages.BorderSizePixel = 0
-	Pages.Position = UDim2.new(0, 90, 0, 15)
-	Pages.Size = UDim2.new(0, 235, 0, 220)
+	Pages.Position = UDim2.new(0, 110, 0, 25)
+	Pages.Size = UDim2.new(0, 265, 0, 280)
 	local PagesFolder = Instance.new("Folder")
 	PagesFolder.Name = "PagesFolder"
 	PagesFolder.Parent = Pages
@@ -75,44 +76,45 @@ function WindowTable:CreateWindow()
 	function TabHandler:CreateTab(tabname)
 		
 		tabname = tabname or "New Tab"
-		local tabButton = Instance.new("TextButton")
-		tabButton.Parent = sidebar
-		tabButton.BackgroundColor3 = Color3.fromRGB(61, 64, 71)
-		tabButton.BorderColor3 = Color3.fromRGB(216, 222, 233)
-		tabButton.Size = UDim2.new(0, 75, 0, 30)
-		tabButton.Font = Enum.Font.Gotham
-		tabButton.Text = tabname
-		tabButton.TextColor3 = Color3.fromRGB(216, 222, 233)
-		tabButton.TextSize = 12.000
-		local NewPage = Instance.new("ScrollingFrame")
-		NewPage.Name = "NewPage"
-		NewPage.Parent = PagesFolder
-		NewPage.Active = true
-		NewPage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		NewPage.BackgroundTransparency = 1.000
-		NewPage.BorderSizePixel = 0
-		NewPage.Size = UDim2.new(1, 0, 1, 0)
-		NewPage.ScrollBarThickness = 6
+		local newTab = Instance.new("TextButton")
+		newTab.Name = "newTab"
+		newTab.Parent = tabHolder
+		newTab.BackgroundColor3 = Color3.fromRGB(61, 64, 71)
+		newTab.BorderColor3 = Color3.fromRGB(216, 222, 233)
+		newTab.Size = UDim2.new(0, 90, 0, 35)
+		newTab.Font = Enum.Font.Gotham
+		newTab.Text = "Tab Button"
+		newTab.TextColor3 = Color3.fromRGB(216, 222, 233)
+		newTab.TextSize = 12.000
+		local newPage = Instance.new("ScrollingFrame")
+		newPage.Name = "newPage"
+		newPage.Parent = PagesFolder
+		newPage.Active = true
+		newPage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		newPage.BackgroundTransparency = 1.000
+		newPage.BorderSizePixel = 0
+		newPage.Size = UDim2.new(1, 0, 1, 0)
+		newPage.ScrollBarThickness = 6
 		local UiListLayout = Instance.new("UIListLayout")
 		UiListLayout.Name = "UiListLayout"
-		UiListLayout.Parent = NewPage
+		UiListLayout.Parent = newPage
 		UiListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
 		UiListLayout.SortOrder = Enum.SortOrder.LayoutOrder
 		UiListLayout.Padding = UDim.new(0, 5)
 		
-		tabButton.MouseButton1Down:Connect(function()
+		newTab.MouseButton1Down:Connect(function()
 			for i,v in next, PagesFolder:GetChildren() do
 				v.Visible = false
 			end
-			NewPage.Visible = true
+			newPage.Visible = true
 			
-			for i,v in next, sidebar:GetChildren() do
+			for i,v in next, tabHolder:GetChildren() do
 				if v:IsA("TextButton") then
-					game.TweenService:Create(v, TweenInfo.new(0.5, Enum.EasingStyle.Sine, Enum.EasingDirection.In), {
+					game.TweenService:Create(v, TweenInfo.new(0.2, Enum.EasingStyle.Sine, Enum.EasingDirection.In), {
 						BackgroundColor3 = Color3.fromRGB(61, 64, 71)}):Play()
 				end
 			end
-			game.TweenService:Create(tabButton, TweenInfo.new(0.5, Enum.EasingStyle.Sine, Enum.EasingDirection.In), {
+			game.TweenService:Create(newTab, TweenInfo.new(0.2, Enum.EasingStyle.Sine, Enum.EasingDirection.In), {
 				BackgroundColor3 = Color3.fromRGB(70, 100, 180)}):Play()
 		end)
 	end
