73d1220d9180a69ac72144dfa39aeb5e2d6e40ff
Griffin // vykzs <89820610+vyks@users.noreply.github.com>
2022-06-07 13:19:28 -0400

    Fixed negative chunks

--- MS2 Chunk&Position.lua
+++ MS2 Chunk&Position.lua
@@ -3,21 +3,23 @@ local pos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
 
 local function chunkfinder()
     local pos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+    chunkx = math.round(pos.X/60)
+    chunkz = math.round(pos.Z/60)
 
     local fakey = pos.Y/60
     if pos.Y > -30 then
         if pos.Y > 0 then
             fakey = fakey + 1
+            chunky = math.abs(fakey)
+            chunky = chunky * -1
         else
             fakey = fakey
+            chunky = math.abs(fakey)
         end
     else
         fakey = fakey
+        chunky = math.abs(fakey)
     end
-
-    chunkx = math.round(pos.X/60)
-    chunky = math.abs(fakey)
-    chunkz = math.round(pos.Z/60)
 end
 chunkfinder()
 
